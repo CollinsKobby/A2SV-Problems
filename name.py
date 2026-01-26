e952f3c6dbff323af99104a05a75184e05276643
Collins Nukporti <134154916+CollinsKobby@users.noreply.github.com>
2026-01-26 14:00:04 +0000

    print_full_name function

---
+++ name.py
@@ -0,0 +1,5 @@
+def print_full_name(first, last):
+    # Write your code here
+    # first = input()
+    # last = input()
+    print("Hello " + first + " " + last + "! You just delved into python.")
