ead490db82c5e8dc24cc5f3706528667b3300830
Collins Nukporti <134154916+CollinsKobby@users.noreply.github.com>
2026-02-09 14:03:59 +0000

    Add isSubset method to check array subset
    Implement subset checking using Counter from collections.

---
+++ array_subset.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        count_a = Counter(a)
+        count_b = Counter(b)
+        
+        for item in count_b:
+            if count_b[item] > count_a[item]:
+                return False
+                
+        return True
