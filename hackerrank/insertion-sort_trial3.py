608544e197f5e472f2c64039080149b7994edd24
Collins Nukporti <134154916+CollinsKobby@users.noreply.github.com>
2026-03-03 13:05:41 +0000

    Add solution for Insertion Sort (trial 3)

---
+++ hackerrank/insertion-sort_trial3.py
@@ -0,0 +1,12 @@
+def insertionSort1(n, arr):
+    value = arr[n - 1]
+    i = n - 2
+
+    while i >= 0 and arr[i] > value:
+        arr[i + 1] = arr[i] 
+        print(' '.join(map(str, arr)))
+        i -= 1
+    arr[i + 1] = value
+    print(' '.join(map(str, arr)))
+       
+    return
